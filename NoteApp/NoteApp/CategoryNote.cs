f3cb5e76faa42191c7f55efe47786b9d8f365d42
Настя Смакотина <smakotina98@mail.ru>
2019-11-09 15:15:07 +0700

    Разработана логика приложения : Note, CategoryNote, Project

---
+++ NoteApp/NoteApp/CategoryNote.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoteApp
+{
+    /// <summary>
+    /// "Категория заметки" (перечисление).
+    /// </summary>
+    public enum CategoryNote
+    {
+        /// <summary>
+        /// Работа
+        /// </summary>
+        Work,
+
+        /// <summary>
+        /// Дом
+        /// </summary>
+        Home,
+
+        /// <summary>
+        /// Здоровье и спорт
+        /// </summary>
+        HeathAndSport,
+
+        /// <summary>
+        /// Люди
+        /// </summary>
+        People,
+
+        /// <summary>
+        /// Документы
+        /// </summary>
+        Documents,
+
+        /// <summary>
+        /// Финансы
+        /// </summary>
+        Finance,
+
+        /// <summary>
+        /// Разное
+        /// </summary>
+        Different
+    }
+
+}
---
+++ NoteApp/NoteApp/Project.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace NoteApp
+{
+    /// <summary>
+    /// Класс, содержащий список всех заметок, созданных в приложении.
+    /// </summary>
+    public class Project
+    {
+        /// <summary>
+        /// Cписок заметок.
+        /// </summary>
+        public static List<Note> Notes;
+
+        /// <summary>
+        /// Возвращает и задает список всех заметок.
+        /// </summary>
+        public List<Note> Note
+        {
+            get
+            {
+                return Notes;
+            }
+            set
+            {
+                Notes = value;
+            }
+        }
+
+        /// <summary>
+        /// Конструктор класса "Проект".
+        /// </summary>
+        public Project()
+        {
+            Note = new List<Note>();
+        }
+
+
+    }
+}
